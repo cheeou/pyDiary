9766476003adea7eedf7f0529d47a89e7bdaa5a3
cheeou <best.coder.jiwoo@gmail.com>
2024-08-23 01:39:14 +0900

    FEAT: 일기 페이지 상세 기능 추가

--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .forms import PageForm
+from .models import Page
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
@@ -28,5 +29,19 @@ class PageCreateView(View):
         return render(request, "diary/page_form.html", {"form": form})
 
 
+class PageListView(View):
+    def get(self, request):
+        page_list = Page.objects.order_by("-page_date")
+        return render(request, "diary/page_list.html", {"page_list": page_list})
+
+
+class PageDetailView(View):
+    def get(self, request, id):
+        page = get_object_or_404(Page, id=id)
+        return render(request, "diary/page_detail.html", {"page": page})
+
+
 index = IndexView.as_view()
 page_create = PageCreateView.as_view()
+page_list = PageListView.as_view()
+page_detail = PageDetailView.as_view()

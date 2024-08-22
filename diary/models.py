b605a0a498ba5209996632483631f3dfc80f2d4b
cheeou <best.coder.jiwoo@gmail.com>
2024-08-23 00:55:52 +0900

    FEAT: 일기 쓰기 기능 추가

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Page(models.Model):
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, verbose_name="ID"
+    )
+    title = models.CharField(max_length=100, verbose_name="제목")
+    body = models.TextField(max_length=2000, verbose_name="내용")
+    page_date = models.DateField(verbose_name="날짜")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="작성일자")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="수정일자")
+
+    def __str__(self):
+        return self.title
--- diary/views.py
+++ diary/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from .forms import PageForm
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 # Create your views here.
 
 
 class IndexView(View):
     def get(self, request):
-        return render(request, "diary/index.html")
+        datetime_now = datetime.now(ZoneInfo("Asia/Seoul")).strftime(
+            "%Y년%m월%d일 %H:%M:%S"
+        )
+        return render(request, "diary/index.html", {"datetime_now": datetime_now})
+
+
+class PageCreateView(View):
+    def get(self, request):
+        form = PageForm()
+        return render(request, "diary/page_form.html", {"form": form})
+
+    def post(self, request):
+        form = PageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("diary:index")
+        return render(request, "diary/page_form.html", {"form": form})
 
 
 index = IndexView.as_view()
+page_create = PageCreateView.as_view()
